84840483ae5899a1cbe7ea11d81629f2bc621800
Andrey from Virtual Win7 <skibaal@modeltech.ru>
2016-11-17 16:42:10 +0200

    Some changes in structure, HTML5 Canvas and JS added

---
+++ core.py
@@ -0,0 +1,55 @@
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array, load_img
+
+from flask import Flask
+
+app = Flask(__name__)
+
+def recognize(img):
+    model = load_model("my_model.h5") 
+    result = model.predict(img)
+    return result
+       
+def max_index(arr):
+    max_val = -1
+    max_i = 0
+    for i in range(len(arr)):
+        if arr[i] > max_val:
+            max_val = arr[i]
+            max_i = i
+    return max_i
+        
+        
+def preprocess_img(image):
+    img = load_img(image)
+    img = img_to_array(img)
+    
+    img /= 255
+    
+    result = []
+    for i in img[0]:
+        for j in i:
+            if j == 0:
+                result.append(1)
+            else:
+                result.append(0)
+    return result
+    
+
+def print_img(img):
+    for i in range(28):
+        string = ""
+        for j in range(28):
+            p = int(img[28*i+j])
+            if p == 0:
+                string += " "
+            else:
+                string += str(p)
+        print(string)    
+
+        
+if __name__ == "__main__":
+    pass
