bf8bd9d21fb34ab5fda88d303c3cd398654db985
Andrey Skiba <a.skiba@dlg.im>
2019-04-14 13:02:26 +0300

    refresh the project

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
-FROM python:latest
+FROM python:3.7
 
 MAINTAINER Skiba Andrey skiba_andrey@mail.ru
 
-RUN pip install keras==1.1.2
-RUN pip install flask==0.11.1
-RUN pip install Pillow==3.3.1
-RUN pip install h5py==2.6.0
-
 ENV KERAS_BACKEND=theano
 
-ADD . /app
+RUN pip install keras flask Pillow h5py theano
+
+WORKDIR /app
+COPY . /app
 
 EXPOSE 5000
-WORKDIR /app
+
 CMD ["python", "core.py"]
--- core.py
+++ core.py
@@ -9,46 +9,34 @@ from PIL import Image
 from io import BytesIO
 
 app = Flask(__name__)
-model = load_model("nn_model.h5") 
+model = load_model("nn_model.h5")
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
     data = request.get_json()
     if data != None:
         img = preprocess_img(data["ImageData"])
-        result = recognize(img) 
+        result = recognize(img)
         return json.dumps({'answer' : str(result)})
     else:
         return render_template('index.html')
 
-
 def recognize(img):
     result = model.predict(img)[0]
-    return max_index(result)
-       
+    return np.argmax(result)
 
-def max_index(arr):
-    max_val = -1
-    max_i = 0
-    for i in range(len(arr)):
-        if arr[i] > max_val:
-            max_val = arr[i]
-            max_i = i
-    return max_i
-        
-        
 def preprocess_img(image):
     imgdata = base64.b64decode(image)
-    
+
     im = Image.open(BytesIO(imgdata))
-   
+
     bg = Image.new("RGB", im.size, (255,255,255))
     bg.paste(im,im)
-    
+
     img = img_to_array(bg)
     img /= 255
     result = []
-    
+
     for i in img:
         for j in i:
             if j[0] == 1:
@@ -56,7 +44,7 @@ def preprocess_img(image):
             else:
                 result.append(1)
     return np.array([result])
-    
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
